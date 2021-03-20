62d3ce99841d09eaa60336aa9f38396b6ce46344
Michael Lluesma <mikepresl@gmail.com>
2021-03-20 16:34:39 +0100

    added validate jwt and authentication filter

--- src/main/java/com/running/app/security/JwtProvider.java
+++ src/main/java/com/running/app/security/JwtProvider.java
@@ -1,9 +1,12 @@
 package com.running.app.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import static io.jsonwebtoken.Jwts.parser;
 
 
 @Service
@@ -18,6 +21,20 @@ public class JwtProvider {
         .setSubject(principal.getUsername())
         .signWith(SignatureAlgorithm.HS256, jwtkey)
         .compact();
+  }
+
+  public boolean validateToken(String jwt) {
+    parser().setSigningKey(jwtkey).parseClaimsJws(jwt);
+    return true;
+  }
+
+  public String getUsernameFromJwt(String token) {
+    Claims claims = parser()
+        .setSigningKey(jwtkey)
+        .parseClaimsJws(token)
+        .getBody();
+
+    return claims.getSubject();
   }
 
 }
