f9ffc46ec98ff8fb02f565f739f7acb6e47b8fb0
Mike Lluesma <mike.lluesma@mews.com>
2025-01-06 15:10:44 +0100

    initial commit

---
+++ Controllers/RunController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessApp.Controllers
+{
+    public class RunController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
