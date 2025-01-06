113f23658a1cedd6fc1522201753748d90ba3541
Mike Lluesma <mike.lluesma@mews.com>
2025-01-06 15:58:14 +0100

    wip

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace FitnessApp
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
