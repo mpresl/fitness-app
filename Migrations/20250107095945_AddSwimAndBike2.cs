e7e7178b75d96c23bdd97af80c8b7d1f14410163
Mike Lluesma <mike.lluesma@mews.com>
2025-01-07 11:07:04 +0100

    added swims and bike activities

---
+++ Migrations/20250107095945_AddSwimAndBike2.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSwimAndBike2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bikes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Distance = table.Column<double>(type: "float", nullable: false),
+                    Time = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bikes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Swims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Distance = table.Column<double>(type: "float", nullable: false),
+                    Time = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Swims", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bikes");
+
+            migrationBuilder.DropTable(
+                name: "Swims");
+        }
+    }
+}
