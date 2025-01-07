474e2093e686536500b01871e3d018717e5edf84
Mike Lluesma <61774872+mpresl@users.noreply.github.com>
2025-01-07 11:28:49 +0100

    Add activity controller and view model (#5)

---
+++ Controllers/ActivityController.cs
@@ -0,0 +1,28 @@
+﻿using FitnessApp.Data;
+using FitnessApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessApp.Controllers
+{
+    public class ActivityController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ActivityController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = new ActivityViewModel
+            {
+                Runs = _context.Runs.ToList(),
+                Swims = _context.Swims.ToList(),
+                Bikes = _context.Bikes.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Models/ActivityViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FitnessApp.Models
+{
+    public class ActivityViewModel
+    {
+        public List<Run> Runs { get; set; }
+        public List<Swim> Swims { get; set; }
+        public List<Bike> Bikes { get; set; }
+    }
+}
